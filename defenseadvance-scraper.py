13c253ecc46362b43fd83b925253acb041fd5671
aylin <lugina.oezdemir@code.berlin>
2023-02-17 13:28:52 +0100

    Scraping Event names

--- defenseadvance-scraper.py
+++ defenseadvance-scraper.py
@@ -2,3 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# getting a response object = "url" - all information is here now
+url= 'https://www.defenseadvancement.com/events/'
+page= requests.get(url)
+
+soup= BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
+
+event_name = [event.text for event in soup.find_all('span', {'class': 'product-card-heading'})]
+
+
+print(event_name)
