48f913828bd99aed2edd78189ae5f95f7f619797
Lugina <lugina.oezdemir@code.berlin>
2023-03-01 14:32:34 +0100

    Get event information

--- defenseadvance-scraper.py
+++ defenseadvance-scraper.py
@@ -15,4 +15,14 @@ for page in range(1,3):
     #get event boxes
     event_box = soup.find_all('span', {'class': 'product-box-details'})
     
-
+    #lop through boxes and extract information
+    for box in event_box:
+        #create dict for each event
+        event_data = {}
+        
+        event_data['event_name'] = event_box.find('span', {'class': 'product-box-heading'}).text.split()
+        event_data['event_day'] = event_box.find('span', {'class': 'wenibar-day'}).text.split()
+        event_data['event_month'] = event_box.find('span', {'class': 'webinar-month'}).text.split()
+        event_data['event_duration'] = event_box.find('span', {'class': 'event_date'}).text.split()
+        event_data['event_location'] = event_box.find('span', {'class': 'event_country'}).text.split()
+        event_data['event_link'] = event_box.find('a')['href']
