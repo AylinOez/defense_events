f9e0e02c4e21ce05d5b7d38da5d067a3cb4b0773
Lugina <lugina.oezdemir@code.berlin>
2023-03-01 14:24:59 +0100

    Loop through page and create soup

--- defenseadvance-scraper.py
+++ defenseadvance-scraper.py
@@ -2,22 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# getting a response object = "url" - all information is here now
+#create list to store events
+events = []
 
+#loop through pages
 for page in range(1,3):
-    url = 'https://www.defenseadvancement.com/events/page/' + str(page)
-    response= requests.get(url)
-    soup= BeautifulSoup(response.content, 'html.parser')
-    event_names= [event.text for event in soup.find_all('span', {'product-card-heading'})]
-    event_start_date= [day.text for day in soup.find_all('span', {'class': 'webinar-day'})]
-    event_month = [month.text for month in soup.find_all('span', {'class': 'webinar-month'})]
-    event_duration= [days.text for days in soup.find_all('span', {'class': 'event-date'})]
-    event_links= [links.find('a')['href'] for links in soup.find_all('span', {'product-card-heading'})]
-    event_location= [location.text for location in soup.find_all('span', {'class': 'event-country'})]
+    #send get request and parse
+    url = f"https://www.defenseadvancement.com/events/page/{page}/"
+    reponse= requests.get(url)
+    soup = BeautifulSoup(reponse.content, 'html.parser')
 
-print(event_names)
-print(event_start_date)
-print(event_month)
-print(event_duration)
-print(event_links)
-print(event_location)
+    #get event boxes
+    event_box = soup.find_all('span', {'class': 'product-box-details'})
+    
+
