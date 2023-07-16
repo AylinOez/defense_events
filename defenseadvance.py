1edab446a751918c6a678fc145f164756bca2ab8
LuginaAylinOezdemir <lugina.oezdemir@code.berlin>
2023-07-16 11:49:36 +0200

    New soup in defensadvance.py, defined tags and attr

--- defenseadvance.py
+++ defenseadvance.py
@@ -7,24 +7,32 @@ from bs4 import BeautifulSoup
 
 
 url = 'https://www.defenseadvancement.com/events/'
-#response = requests.get(url)
-#soup = BeautifulSoup(response.content, 'html.parser')
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 #page_class = 'page-numbers'
-page_tag= 'div'
-page_attribute= 'pagination'
-soup = making_soup(url, page_tag, page_attribute, max_pages=10)
 
-event_box = find_event_box(soup, 'span', {'class': 'product-box-details'})
-events = event_info(event_box, 'span', {'class': 'product-card-heading'}, 'span', {'class': 'webinar-day'},
-                    'span', {'class': 'webinar-month'}, 'span', {'class': 'event-country'})
+#soup = making_soup(url)
+tag_name = day_tag = month_tag = location_tag = name_tag = 'span'
+class_name = 'product-box-details'
+
+name_attribute = 'product-card-heading'
+day_attribute = 'webinar-day'
+month_attribute = 'webinar-month'
+location_attribute = 'event-country'
+
+event_box = find_event_box(soup, tag_name, class_name)
+print(event_box)
+
+events = event_info(event_box, name_tag, name_attribute, day_tag, day_attribute,
+                    month_tag, month_attribute, location_tag, location_attribute)
 print(events)
 
 
-with open('defenseadvance.csv', 'a', newline='') as csvfile:
-    fieldnames= ['event_name', 'event_day', 'event_month', 'event_location', 'event_link']
+# with open('defenseadvance.csv', 'a', newline='') as csvfile:
+#     fieldnames= ['event_name', 'event_day', 'event_month', 'event_location', 'event_link']
     
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
 
-    for row in events:
-        writer.writerow(row)
+#     for row in events:
+#         writer.writerow(row)
