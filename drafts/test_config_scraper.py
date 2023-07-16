9dde6f3189b7a7832893737b0c4311d81d6d4b25
Gina <64898549+LuginaAylinOezdemir@users.noreply.github.com>
2023-07-16 13:48:07 +0200

    Created first test for making soup

--- drafts/test_config_scraper.py
+++ drafts/test_config_scraper.py
@@ -1,5 +1,11 @@
 import pytest
 from config_scraper import *
 
-
+def test_making_soup():
+    url = 'https://www.defenseadvancement.com/events/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    assert soup == making_soup(url)
+    
+    
 
