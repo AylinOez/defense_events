0e0f11dc42685785b1115248766026efdc0643ee
Gina <64898549+LuginaAylinOezdemir@users.noreply.github.com>
2023-09-30 08:19:54 +0200

    Created function for control html and html file

--- drafts/test.py
+++ drafts/test.py
@@ -1,6 +1,9 @@
+import unittest
 import requests
 from bs4 import BeautifulSoup
 from scraper_config import *
+from defenseadvance import *
+
 
 
 for page in range(1,3):
@@ -8,6 +11,10 @@ for page in range(1,3):
     url = f"https://www.defenseadvancement.com/events/page/{page}/"
     reponse= requests.get(url)
     soup = BeautifulSoup(reponse.content, 'html.parser')
+    
+class TestDefenseAdvanceSoup(unittest.TestCase):
+    def test_soup(self):
+        self.assertEqual(test_soup == soup.prettify(), True)
 
     event_box = find_event_box(soup, 'span', {'class': 'product-box-details'})
 
